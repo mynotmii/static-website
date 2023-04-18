48aa03fd2cd69ccb83a01974a3889e5fda9ff42d
Myie <myngghuynh@gmail.com>
2023-04-18 16:49:52 +0700

    test

---
+++ script.js
@@ -0,0 +1,26 @@
+function changeImage(x,image){
+    if(x == 1){
+        image.src = 'img/rocking-horse.gif'
+    }
+    if(x == 2){
+        image.src = 'img/rocking-horse.png'
+    }
+    if(x == 3){
+        image.src = 'img/rock-star.gif'
+    }
+    if(x == 4){
+        image.src = 'img/rock-star.png'
+    }
+    if(x == 5){
+        image.src = 'img/globe-earth.gif'
+    }
+    if(x == 6){
+        image.src = 'img/globe-earth.png'
+    }
+    if(x == 7){
+        image.src = 'img/books.gif'
+    }
+    if(x == 8){
+        image.src = 'img/books.png'
+    }
+}
