e50fae88e6f0365fc13eda907951d04788681441
Myie <myngghuynh@gmail.com>
2023-04-19 11:28:42 +0700

    child outline + other categories page

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function changeImage(x,image){
     if(x == 1){
-        image.src = 'img/rocking-horse.gif'
+        image.src = '/static-website/img/rocking-horse.gif'
     }
     if(x == 2){
-        image.src = 'img/rocking-horse.png'
+        image.src = '/static-website/img/rocking-horse.png'
     }
     if(x == 3){
-        image.src = 'img/rock-star.gif'
+        image.src = '/static-website/img/rock-star.gif'
     }
     if(x == 4){
-        image.src = 'img/rock-star.png'
+        image.src = '/static-website/img/rock-star.png'
     }
     if(x == 5){
-        image.src = 'img/globe-earth.gif'
+        image.src = '/static-website/img/globe-earth.gif'
     }
     if(x == 6){
-        image.src = 'img/globe-earth.png'
+        image.src = '/static-website/img/globe-earth.png'
     }
     if(x == 7){
-        image.src = 'img/books.gif'
+        image.src = '/static-website/img/books.gif'
     }
     if(x == 8){
-        image.src = 'img/books.png'
+        image.src = '/static-website/img/books.png'
     }
 }
