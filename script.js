f474af16066f4fb5f839b1cd33ef23c190600f0a
Myie <myngghuynh@gmail.com>
2023-04-20 00:13:47 +0700

    navbar works in both devices

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-function changeImage(x,image){
-    if(x == 1){
-        image.src = '/static-website/img/rocking-horse.gif'
-    }
-    if(x == 2){
-        image.src = '/static-website/img/rocking-horse.png'
-    }
-    if(x == 3){
-        image.src = '/static-website/img/rock-star.gif'
-    }
-    if(x == 4){
-        image.src = '/static-website/img/rock-star.png'
-    }
-    if(x == 5){
-        image.src = '/static-website/img/globe-earth.gif'
-    }
-    if(x == 6){
-        image.src = '/static-website/img/globe-earth.png'
-    }
-    if(x == 7){
-        image.src = '/static-website/img/books.gif'
-    }
-    if(x == 8){
-        image.src = '/static-website/img/books.png'
-    }
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar-items');
+
+if (bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active')
+    })
+}
+
+if (close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
 }
